31b56a76274e35e1b3f4cb11c3b28fe041b5ed0f
Cristian Cavanzo <cristian.frontendev@gmail.com>
2023-06-07 06:16:09 -0500

    [Add]: localstorage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
@@ -6,18 +6,47 @@ import { CreateTodoButton } from './CreateTodoButton';
 import { TodoItem } from './TodoItem';
 import { TodoHead } from './TodoHead';
 
-const defaultTodos = [
-	{ text: 'Cortar cebolla', completed: true },
-	{ text: 'Tomar el curso de intro a React', completed: false },
-	{ text: 'Llorar con la llorona', completed: false },
-	{ text: 'LALALA', completed: false },
-];
+interface Todos {
+	text: string;
+	completed: boolean;
+}
+// const defaultTodos = [
+// 	{ text: 'Cortar cebolla', completed: true },
+// 	{ text: 'Tomar el curso de intro a React', completed: false },
+// 	{ text: 'Llorar con la llorona', completed: false },
+// 	{ text: 'LALALA', completed: false },
+// ];
+// localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos))
 
 const Home = () => {
+	const nameTodos = 'TODOS_V1';
+	// localStorage.getItem('TODOS_V1', defaultTodos)
+	// localStorage.removeItem('TODOS_V1');
+
 	const [state, setState] = useState({
 		search: '',
-		todos: defaultTodos,
+		todos: [],
 	});
+	useEffect(() => {
+		// eslint-disable-next-line no-undef
+		if (globalThis.window) {
+			let parsedTodos: Todos[] = JSON.parse(localStorage.getItem(nameTodos));
+			if (!parsedTodos) {
+				localStorage.setItem('TODOS_V1', JSON.stringify([]));
+				parsedTodos = [];
+			}
+			setState({
+				...state,
+				todos: parsedTodos,
+			});
+		}
+	}, []);
+
+	const saveTodos = (newTodos: Todos[]) => {
+		localStorage.setItem(nameTodos, JSON.stringify(newTodos));
+		setState({ ...state, todos: newTodos });
+	};
+
 	const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
 		const value = event.target.value;
 		setState({
@@ -38,18 +67,12 @@ const Home = () => {
 	const completeTodo = (id: number) => {
 		const newTodos = [...state.todos];
 		newTodos[id].completed = true;
-		setState({
-			...state,
-			todos: newTodos,
-		});
+		saveTodos(newTodos);
 	};
 	const deleteTodo = (id: number) => {
 		const newTodos = [...state.todos];
 		newTodos.splice(id, 1);
-		setState({
-			...state,
-			todos: newTodos,
-		});
+		saveTodos(newTodos);
 	};
 
 	return (
